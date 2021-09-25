04efad93a874338d9497b88f7f954b23b8f02214
Vinicius do Prado <vinicios.prado@hotmail.com>
2021-09-25 13:31:51 -0300

    Conclusão desafio rockeatseat

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,26 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    };
+    setTasks( oldTasks => [...oldTasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks: Task[] = tasks.map(task => {
+      if(task.id === id){
+        task.done = !task.done
+      }
+      return task;
+    });
+    setTasks([...updatedTasks]);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter((item)=>item.id !== id));
   }
 
   return (
